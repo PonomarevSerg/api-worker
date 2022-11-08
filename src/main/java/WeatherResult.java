3e95b375ae73e56191461ec8c3ccbc5ab54db4f2
PonomarevSerg <r0xd@yandex.ru>
2022-11-08 19:11:37 +0300

    Add weather api.

---
+++ src/main/java/WeatherResult.java
@@ -0,0 +1,47 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WeatherResult {
+    private String date;
+    private String temperature;
+    private final String city = "MOSCOW";
+    private String weather;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherResult{" +
+                "date='" + date + '\'' +
+                ", temperature='" + temperature + '\'' +
+                ", city='" + city + '\'' +
+                ", weather='" + weather + '\'' +
+                '}';
+    }
+}
